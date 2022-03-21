f9aebb4f9981d0af282d316584ecc817cec9324f
longrunsoul <longrunsoul@outlook.com>
2022-03-21 17:38:41 +0800

    add get_last_period_days(..), get_next_period_days(..) to revisit_planner.rs

--- src/revisit_planner.rs
+++ src/revisit_planner.rs
@@ -2,3 +2,19 @@
 pub fn get_init_period_days() -> u16 {
     1
 }
+
+pub fn get_last_period_days(current_period_days: u16) -> u16 {
+    if current_period_days == 1 {
+        return 1;
+    }
+
+    current_period_days / 2
+}
+
+pub fn get_next_period_days(current_period_days: u16) -> u16 {
+    if current_period_days == 128 {
+        return 128;
+    }
+
+    current_period_days * 2
+}
