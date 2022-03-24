32b6fa9114e1641cf697adfd14c23d37070257a6
longrunsoul <longrunsoul@outlook.com>
2022-03-24 21:39:47 +0800

    add code for Syncer::get_data() in infra/syncer.rs

--- src/infra/mod.rs
+++ src/infra/mod.rs
@@ -2,8 +2,10 @@ mod sql_value;
 mod model;
 mod db;
 mod secure_keys;
+mod syncer;
 
 pub use sql_value::*;
 pub use model::*;
 pub use db::*;
 pub use secure_keys::*;
+pub use syncer::*;
--- src/infra/secure_keys.rs
+++ src/infra/secure_keys.rs
@@ -5,7 +5,7 @@ const KEYRING_SERVICE: &str = "wordmem.lrs";
 
 #[derive(Debug, PartialEq)]
 pub struct SyncKeys {
-    pub email: String,
+    pub email_domain: String,
     pub username: String,
     pub password: String,
 }
@@ -13,14 +13,14 @@ pub struct SyncKeys {
 impl SyncKeys {
     fn try_get_keys() -> Result<SyncKeys> {
         Ok(SyncKeys {
-            email: keyring::Entry::new(KEYRING_SERVICE, "email").get_password()?,
+            email_domain: keyring::Entry::new(KEYRING_SERVICE, "email_domain").get_password()?,
             username: keyring::Entry::new(KEYRING_SERVICE, "username").get_password()?,
             password: keyring::Entry::new(KEYRING_SERVICE, "password").get_password()?,
         })
     }
 
     pub fn clear_keys() -> Result<()> {
-        let _ = keyring::Entry::new(KEYRING_SERVICE, "email").delete_password();
+        let _ = keyring::Entry::new(KEYRING_SERVICE, "email_domain").delete_password();
         let _ = keyring::Entry::new(KEYRING_SERVICE, "username").delete_password();
         let _ = keyring::Entry::new(KEYRING_SERVICE, "password").delete_password();
 
@@ -39,7 +39,7 @@ impl SyncKeys {
     }
 
     pub fn set_keys(&self) -> Result<()> {
-        keyring::Entry::new(KEYRING_SERVICE, "email").set_password(&self.email)?;
+        keyring::Entry::new(KEYRING_SERVICE, "email_domain").set_password(&self.email_domain)?;
         keyring::Entry::new(KEYRING_SERVICE, "username").set_password(&self.username)?;
         keyring::Entry::new(KEYRING_SERVICE, "password").set_password(&self.password)?;
 
