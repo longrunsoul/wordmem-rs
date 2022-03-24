8410eb1c39c0b70f25f19a9b8c6c36b12f36aaec
longrunsoul <longrunsoul@outlook.com>
2022-03-24 16:13:32 +0800

    add code for infra/secure_keys.rs

--- src/infra/mod.rs
+++ src/infra/mod.rs
@@ -1,7 +1,9 @@
 mod sql_value;
 mod model;
 mod db;
+mod secure_keys;
 
 pub use sql_value::*;
 pub use model::*;
 pub use db::*;
+pub use secure_keys::*;
---
+++ src/infra/secure_keys.rs
@@ -0,0 +1,48 @@
+use anyhow::Result;
+use keyring;
+
+const KEYRING_SERVICE: &str = "wordmem.lrs";
+
+#[derive(Debug, PartialEq)]
+pub struct SyncKeys {
+    pub email: String,
+    pub username: String,
+    pub password: String,
+}
+
+impl SyncKeys {
+    fn try_get_keys() -> Result<SyncKeys> {
+        Ok(SyncKeys {
+            email: keyring::Entry::new(KEYRING_SERVICE, "email").get_password()?,
+            username: keyring::Entry::new(KEYRING_SERVICE, "username").get_password()?,
+            password: keyring::Entry::new(KEYRING_SERVICE, "password").get_password()?,
+        })
+    }
+
+    pub fn clear_keys() -> Result<()> {
+        let _ = keyring::Entry::new(KEYRING_SERVICE, "email").delete_password();
+        let _ = keyring::Entry::new(KEYRING_SERVICE, "username").delete_password();
+        let _ = keyring::Entry::new(KEYRING_SERVICE, "password").delete_password();
+
+        Ok(())
+    }
+
+    pub fn get_keys() -> Result<Option<SyncKeys>> {
+        let keys = SyncKeys::try_get_keys();
+        if keys.is_err() {
+            SyncKeys::clear_keys()?;
+            return Ok(None)
+        }
+
+        let keys = keys.unwrap();
+        Ok(Some(keys))
+    }
+
+    pub fn set_keys(&self) -> Result<()> {
+        keyring::Entry::new(KEYRING_SERVICE, "email").set_password(&self.email)?;
+        keyring::Entry::new(KEYRING_SERVICE, "username").set_password(&self.username)?;
+        keyring::Entry::new(KEYRING_SERVICE, "password").set_password(&self.password)?;
+
+        Ok(())
+    }
+}
